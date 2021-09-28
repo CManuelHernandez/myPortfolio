6513d1de34a2edafec759f28b81e5eaffec09e3c
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-09-28 18:45:02 +0200

    Feat: Translation

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import logo from "../../logo.svg";
 import Layout from "../layout/Layout";
+import { useTranslation } from "react-i18next";
 
-const MainPage = () => {
+export const MainPage = () => {
+  const { t } = useTranslation("global");
+
   return (
     <Layout>
       <div className="App">
         <header className="App-header">
+          <h1>{t("hello-world")}</h1>
           <img src={logo} className="App-logo" alt="logo" />
         </header>
       </div>
--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import "./Header.css";
 
 const Header = () => {
+  const { i18n } = useTranslation("global");
   return (
     <header>
       <p>Cabecera de la página</p>
+      <button onClick={() => i18n.changeLanguage("es")}>ES</button>
+      <button onClick={() => i18n.changeLanguage("en")}>EN</button>
     </header>
   );
 };
