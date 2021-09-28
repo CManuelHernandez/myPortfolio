6faac2d9ef6356792a09c24cbe9818e8becbf16c
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-09-28 23:39:13 +0200

    Feat: small changes

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { NavBar } from "../../NavBar/NavBar";
-import "./Header.css";
+import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header>
+    <header className={classes.Header}>
       <NavBar />
     </header>
   );
