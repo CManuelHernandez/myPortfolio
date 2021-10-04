82bb43c82ff56e8839081b6997724d04372c5ed6
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-10-04 13:25:09 +0200

    Feat: Footer Traslations

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -3,17 +3,22 @@ import { Container, Row, Col } from "react-bootstrap";
 import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import { CgSoftwareDownload } from "react-icons/cg";
+import { useTranslation } from "react-i18next";
 import pdf from "../../../Assets/CManuelHM-CV.pdf";
 import classes from "./Footer.module.css";
 
 function Footer() {
   let date = new Date();
   let year = date.getFullYear();
+  const { t } = useTranslation("global");
   return (
     <Container fluid className={classes.footer}>
       <Row>
         <Col md="4" className={classes.footerCopywright}>
-          <h3>Designed and Developed by Carlos Manuel Hernández Montero</h3>
+          <h3>
+            {t("footer.Designed and Developed by")} Carlos Manuel Hernández
+            Montero
+          </h3>
           <h3>Copyright © {year} CManuelHM</h3>
         </Col>
         <Col md="4" className={classes.footerCopywright}>
@@ -26,7 +31,7 @@ function Footer() {
             <CgSoftwareDownload
               style={{ fontSize: "1.6em", marginRight: "0.5rem" }}
             />
-            Download My CV
+            {t("footer.Download My CV")}
           </a>
         </Col>
         <Col md="4" className={classes.footerBody}>
