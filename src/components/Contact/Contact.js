0cfdb5e701f7e6b55575773bbeb1d58c4c8a93e9
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-10-06 18:42:49 +0200

    Feat: Some Links for the contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,11 +4,15 @@ import { MdEmail, MdLocalPhone } from "react-icons/md";
 import { GrLinkedinOption } from "react-icons/gr";
 import { FiGithub } from "react-icons/fi";
 import Layout from "../layout/Layout";
+import { Link } from "react-router-dom";
 import ContactInfoItem from "./ContactInfoItem";
 import { useTranslation } from "react-i18next";
 
 import classes from "./Contact.module.css";
 import ContactForm from "./ContactForm";
+
+const googleMaps =
+  "https://www.google.es/maps/place/Rivas-Vaciamadrid,+Madrid/@40.3493693,-3.6740024,11z/data=!4m5!3m4!1s0xd423c0de6c9b919:0x9014cc03cd201292!8m2!3d40.3519065!4d-3.5357334?hl=es";
 
 export const Contact = () => {
   const { t } = useTranslation("global");
@@ -34,18 +38,31 @@ export const Contact = () => {
                   icon={<MdEmail />}
                   text="cmnauelhm87@gmail.com"
                 />
-
-                <ContactInfoItem
-                  icon={<FiGithub />}
-                  text="github.com/CManuelHernandez"
-                />
-                <ContactInfoItem
-                  icon={<GrLinkedinOption />}
-                  text="linkedin.com/in/carlosmanuelhernandezmontero"
-                />
-                <ContactInfoItem
-                  text={t("contact.Madrid, Rivas-Vaciamadrid - Spain")}
-                />
+                <a
+                  href="https://github.com/CManuelHernandez"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <ContactInfoItem
+                    icon={<FiGithub />}
+                    text="github.com/CManuelHernandez"
+                  />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/carlosmanuelhernandezmontero/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <ContactInfoItem
+                    icon={<GrLinkedinOption />}
+                    text="linkedin.com/in/carlosmanuelhernandezmontero"
+                  />
+                </a>
+                <a href={googleMaps} target="_blank" rel="noreferrer">
+                  <ContactInfoItem
+                    text={t("contact.Madrid, Rivas-Vaciamadrid - Spain")}
+                  />
+                </a>
               </Col>
               <Col md={6} className={classes.contactForm}>
                 <ContactForm />
